c3a1d5775cd074b2ef1fc6cac971d73d967285be
Dist bit <distbit@pm.me>
2023-01-10 01:57:50 +1100

    first

---
+++ open.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DISPLAY=:1
+brave-browser "file:///home/pimania/Syncs/Obsidian/To%20do/0Prioritised%20Todo.md" --no-sandbox
+#brave-browser "file:///home/pimania/Syncs/Camera/alarmqrcode/alarmqrcode.png" --no-sandbox
